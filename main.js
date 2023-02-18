28736d88c010b0be190e5ea5d6ebd27fc55e15ed
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-17 21:03:06 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let variableA = Number(prompt("Ingrese un número para la variable A"));
-let variableB = Number(prompt("Ingrese un número para la variable B"));
-let variableC = Number(prompt("Ingrese un número para la variable C"));
+console.log(`LISTADO DE LOS PRIMEROS 100 PARES`);
+let j = 1;
 
-console.log(`El número mayor entre ${variableA}, ${variableB} y ${variableC} es:  ${Math.max(variableA,variableB,variableC)}`);
-console.log(`El número menor entre ${variableA}, ${variableB} y ${variableC} es:  ${Math.min(variableA,variableB,variableC)}`);
-
+for(let i = 2; i <= 200; i += 2){
+    console.log(`${j}. ${i}`);
+    j++;
+}
 
 
