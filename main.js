9ff20cd1ac5c61ad59496d1d0a40f66d7f249579
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-17 21:32:19 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-console.log(`LISTADO DE LOS PRIMEROS 100 PARES`);
-let j = 1;
+let continuar = true;
+let numPar = 0;
+let numImpar = 1;
+let numActual;
+let mensajeSuma = "Los números pares ingresados son: ";
+let mensajeMultip = "Los números impares ingresados son: ";
 
-for(let i = 2; i <= 200; i += 2){
-    console.log(`${j}. ${i}`);
-    j++;
+while(continuar){
+    numActual = Number(prompt("Ingrese un número"));
+
+    if(numActual % 2 == 0){
+        numPar += numActual;
+        mensajeSuma += `, ${numActual} `
+    }else{
+        numImpar = numImpar * numActual;
+        mensajeMultip += `, ${numActual} `
+    }
+    continuar = Number(prompt("Si desea continuar presione 1, si no, presione 0"));
 }
 
-
+alert(`${mensajeSuma} y su suma es = ${numPar}.\n${mensajeMultip} y su multiplicación es = ${numImpar}`);
